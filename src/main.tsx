f55c0af8902f5ba1280fc34c9678ac83d80695ec
Roman <r.rybkin94@gmail.com>
2024-01-06 17:33:11 +0500

    project setup

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,11 @@ import { createRoot } from 'react-dom/client'
 
 import { App } from './App'
 
+import './styles/index.scss'
+
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+
 createRoot(document.getElementById('root') as HTMLElement).render(
     <StrictMode>
         <App />
