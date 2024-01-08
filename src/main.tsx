ab3ace239333cd9311d886666875462fe54c5e88
Roman <r.rybkin94@gmail.com>
2024-01-09 03:33:14 +0500

    finish

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
+import {StrictMode} from 'react'
 
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 
-import { App } from './App'
+import {App} from './App'
 
 import './styles/index.scss'
 
@@ -10,9 +10,12 @@ import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import '@fontsource/roboto/500.css'
 import '@fontsource/roboto/300.css'
+import {HashRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root') as HTMLElement).render(
     <StrictMode>
-        <App />
+        <HashRouter>
+            <App/>
+        </HashRouter>
     </StrictMode>
 )
